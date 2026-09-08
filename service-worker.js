2260e1c1ca6b01bf0847309ecaab466e4b3c3f76
YatVFR <41767344+YatVFR@users.noreply.github.com>
2026-09-08 14:34:53 +0800

    Add offline service worker

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='moneygowhere-v1';
+const CORE=['./','./index.html','./style.css','./app.js','./manifest.json','./assets/icons/icon.svg'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
